4a741275f56d26e56698b3aed76122ea13155cef
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-10 06:16:17 +0000

    fix backend api issue

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -49,7 +49,7 @@ ItemSchema.methods.toJSONFor = function(user) {
     slug: this.slug,
     title: this.title,
     description: this.description,
-    image: this.image,
+    image: this.image || "/placeholder.png",
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
