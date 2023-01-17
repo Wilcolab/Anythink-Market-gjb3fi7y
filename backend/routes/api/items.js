704d7bf84a12f8dfb52bd4a5ba8d52c20e5af233
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-17 12:17:31 +0000

    update condition for checking empty image

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -152,8 +152,8 @@ router.post("/", auth.required, function(req, res, next) {
 
       var item = new Item(req.body.item);
       
-      if(item.image.length===0){
-        const imageGenerated = await openai.createCompletion({
+      if(!item?.image){
+        const imageGenerated = await openai.createImage({
           prompt: item.title,
           n: 1,
           size: "256x256",
