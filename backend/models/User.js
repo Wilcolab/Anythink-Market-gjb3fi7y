de11fdcf37c98f0211c4be59071aa4634b821290
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-17 19:21:12 +0000

    add field to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,7 +32,11 @@ var UserSchema = new mongoose.Schema(
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
-    salt: String
+    salt: String,
+    isVerified: {
+      type: Boolean,
+      default: false
+    }
   },
   { timestamps: true }
 );
