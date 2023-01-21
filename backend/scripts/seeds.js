78541f9676699e888f789316f96eeb6353fbcc2d
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-21 12:11:26 +0000

    add seed data

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,41 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+
+import mongoose from "mongoose";
+var User = mongoose.model("User");
+let Item = mongoose.model("Item");
+let Comment = mongoose.model("Comment");
+
+const { faker } = require('@faker-js/faker');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+async function insertFakeRecords(){
+    const name = faker.random.alphanumeric(7);
+    const email = faker.internet.email();
+
+    let newUser = new User({
+        username: name,
+        email: email
+    });
+
+    await newUser.save().then(console.log).catch(console.error);
+
+    let newItem = new Item({
+        title: faker.random.word()
+    });
+    await newItem.save().then(console.log).catch(console.error);
+
+    let newComment = new Comment({
+        body: faker.random.words()
+    });
+
+    await newComment.save().then(console.log).catch(console.error);
+}
+
+let promises = []
+
+for(let i=0; i<100; i++){
+    promises.push(insertFakeRecords());
+}
+
+Promise.all(promises).then(()=>mongoose.disconnect());
