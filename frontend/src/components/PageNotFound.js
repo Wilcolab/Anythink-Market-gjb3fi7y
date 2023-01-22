51d860d42db8d4279fb8e64b5f5b4b4854b25aaf
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-22 17:27:42 +0000

    update message

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -3,7 +3,8 @@ import React from "react";
 const PageNotFound = ()=>{
     return(
         <div>
-            Page not found
+            <h2>404 Page Not Found</h2>
+            <p>Oops, Looks like you've stumbled upon a item that currently does not exist. But don't worry, there are plenty of other cool items to choose from in the Anythink marketplace. Take a look around and find something new!</p>
         </div>
     )
 }
