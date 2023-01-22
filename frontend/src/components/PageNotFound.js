a7cfe5691738108c32668cf30b4b1109b65ec681
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-22 18:52:07 +0000

    add r

---
+++ frontend/src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageNotFound = ()=>{
+    return(
+        <div className="h-s">
+            <h2>404 Page Not Found</h2>
+            <p>Oops, Looks like you've stumbled upon a item that currently does not exist. But don't worry, there are plenty of other cool items to choose from in the Anythink marketplace. Take a look around and find something new!</p>
+        </div>
+    )
+}
+
+export default PageNotFound;
+
