71d82e7f05f2ccab3ebba5a84d4cedd59eb3bda5
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-23 13:32:44 +0000

    add private route

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Route, Redirect} from "react-router-dom";
+
+const PrivateRoute = ({component: Component, currentUser, ...rest})=>{
+    return(
+        <Route 
+            {...rest}
+            render={props=>
+                currentUser ? (
+                    <Component {...props} />
+                ):
+                (
+                    <Redirect to={{pathname: "/login"}}/>
+                )
+            }
+        />
+    )
+}
+
+export default PrivateRoute;
