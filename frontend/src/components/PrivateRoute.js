2a01eea028849c88b1c464c741b0302201f54f9d
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-23 10:40:46 +0000

    update route directory

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {Route, Redirect} from "react-router-dom";
 
-const PrivateRoute = ({component: Component, currentUser, ...rest})=>{
+const PrivateRoute = ({component: Component, ...rest})=>{
+    let currentUser = true;
     return(
         <Route 
             {...rest}
