2b44989f503a8cf9bd4c2c9d5d14a9c50eb51c02
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-25 06:23:34 +0000

    add link to home

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const PageNotFound = ()=>{
     return(
         <div>
             <p>Page Not Found</p>
+            <Link to="/home">Return</Link>
         </div>
     )
 }
