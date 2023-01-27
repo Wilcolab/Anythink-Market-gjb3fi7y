cc9574c64d74ceb91f5fe90cf119b30b85c7819d
Shweta Kale <44832446+raibove@users.noreply.github.com>
2023-01-27 07:52:49 +0000

    use private route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,7 +14,7 @@ import Register from "./Register";
 import Settings from "./Settings";
 import { store } from "../store";
 import { push } from "react-router-redux";
-
+import PrivateRoute from "./PrivateRoute";
 const mapStateToProps = (state) => {
   return {
     appLoaded: state.common.appLoaded,
@@ -61,7 +61,7 @@ class App extends React.Component {
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/editor/:slug" component={Editor} />
-            <Route path="/editor" component={Editor} />
+            <PrivateRoute currentUser={this.props.currentUser} path="/editor" component={Editor} />
             <Route path="/item/:id" component={Item} />
             <Route path="/settings" component={Settings} />
             <Route path="/@:username/favorites" component={ProfileFavorites} />
